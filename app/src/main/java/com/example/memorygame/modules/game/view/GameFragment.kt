26831e58fe4ec4bc4fdf6b7e596ec8f655865c81
Thais Lins <thaislin@gmail.com>
2019-09-21 17:23:26 -0400

    Add choose card logic to GameViewModel and use observer to check if layout has to be updated

--- app/src/main/java/com/example/memorygame/modules/game/view/GameFragment.kt
+++ app/src/main/java/com/example/memorygame/modules/game/view/GameFragment.kt
@@ -1,13 +1,12 @@
 package com.example.memorygame.modules.game.view
 
-
 import android.os.Bundle
-import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.memory_game.modules.card.model.Card
 import com.example.memory_game.modules.product.model.Product
@@ -15,15 +14,11 @@ import com.example.memorygame.R
 import com.example.memorygame.adapter.CardAdapter
 import com.example.memorygame.databinding.FragmentGameBinding
 import com.example.memorygame.modules.game.viewmodel.GameViewModel
-import kotlinx.android.synthetic.main.fragment_game.view.*
-import kotlinx.android.synthetic.main.item_card.view.*
 import java.util.*
 
 class GameFragment : Fragment() {
 
     private var cards = mutableListOf<Card>()
-    private var posFirstCardFaceUp: Int = -1
-    private var posSecondCardFaceUp: Int = -1
     private lateinit var binding: FragmentGameBinding
     private val gameViewModel: GameViewModel? by lazy {
         ViewModelProviders.of(this).get(GameViewModel::class.java)
@@ -43,7 +38,6 @@ class GameFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val products: ArrayList<Product>? = arguments?.getParcelableArrayList("key_product_list")
-        binding.viewModel?.createCards(8, 2, products)
         binding.viewModel?.createCards(10, 2, products)
         setClickListener()
     }
@@ -52,38 +46,11 @@ class GameFragment : Fragment() {
         binding.gridView.setOnItemClickListener(AdapterView.OnItemClickListener { parent, view, position, id ->
             // Get the GridView selected/clicked item text
             val adapter = binding.gridView.adapter as CardAdapter
+            binding.viewModel?.chooseCard(position, adapter)
 
-            if (posSecondCardFaceUp == -1) chooseCard(position, adapter)
+            gameViewModel?.updateLayout?.observe(this, Observer {
+                if (it) adapter.notifyDataSetChanged()
+            })
         })
-    }
-
-    private fun chooseCard(position: Int, adapter: CardAdapter) {
-        if (!cards[position].isMatched && posFirstCardFaceUp != -1) {
-            posSecondCardFaceUp = position
-            cards[posSecondCardFaceUp].isFaceUp = true
-            adapter.notifyDataSetChanged()
-
-            Handler().postDelayed({
-                checkIfCardsMatch(adapter)
-            }, 900)
-        } else {
-            posFirstCardFaceUp = position
-            cards[posFirstCardFaceUp].isFaceUp = true
-            adapter.notifyDataSetChanged()
-        }
-    }
-
-    private fun checkIfCardsMatch(adapter: CardAdapter) {
-        if (cards[posFirstCardFaceUp].id == cards[posSecondCardFaceUp].id) {
-            cards[posFirstCardFaceUp].isMatched = true
-            cards[posSecondCardFaceUp].isMatched = true
-        }
-
-        cards[posFirstCardFaceUp].isFaceUp = false
-        cards[posSecondCardFaceUp].isFaceUp = false
-
-        posFirstCardFaceUp = -1
-        posSecondCardFaceUp = -1
-        adapter.notifyDataSetChanged()
     }
 }
--- app/src/main/java/com/example/memorygame/modules/game/viewmodel/GameViewModel.kt
+++ app/src/main/java/com/example/memorygame/modules/game/viewmodel/GameViewModel.kt
@@ -1,10 +1,12 @@
 package com.example.memorygame.modules.game.viewmodel
 
+import android.os.Handler
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.memory_game.modules.card.model.Card
 import com.example.memory_game.modules.product.model.Image
 import com.example.memory_game.modules.product.model.Product
+import com.example.memorygame.adapter.CardAdapter
 import com.example.memorygame.modules.game.model.datasource.GameDataSourceImp
 import com.example.memorygame.modules.game.model.repository.GameRepository
 import org.koin.core.KoinComponent
@@ -15,6 +17,9 @@ class GameViewModel : ViewModel(), KoinComponent {
         GameRepository(GameDataSourceImp())
     }
 
+    val posFirstCardFaceUp = MutableLiveData<Int>().apply { value = -1 }
+    val posSecondCardFaceUp = MutableLiveData<Int>().apply { value = -1 }
+    val updateLayout = MutableLiveData<Boolean>().apply { value = false }
     val cards = MutableLiveData<List<Card>>().apply { value = emptyList() }
 
     fun getImageList(products: List<Product>?): List<Image> {
@@ -32,5 +37,39 @@ class GameViewModel : ViewModel(), KoinComponent {
         var images = getImageList(products)
         images = images.shuffled().subList(0, pairs)
         cards.value = repository.showCards(pairs, amountMatches, images)
+    }
+
+    fun chooseCard(position: Int, adapter: CardAdapter) {
+        if (posSecondCardFaceUp.value == -1) {
+            if (!cards.value?.get(position)?.isMatched!! && posFirstCardFaceUp.value != -1) {
+                posSecondCardFaceUp.value = position
+                cards.value?.get(position)?.isFaceUp = true
+                updateLayout.value = true
+
+                Handler().postDelayed({
+                    updateLayout.value = false
+                    checkIfCardsMatch(adapter)
+                }, 900)
+            } else {
+                posFirstCardFaceUp.value = position
+                cards.value?.get(position)?.isFaceUp = true
+                updateLayout.value = true
+            }
+        }
+    }
+
+    private fun checkIfCardsMatch(adapter: CardAdapter) {
+        if (cards.value?.get(posFirstCardFaceUp.value!!)?.id == cards.value?.get(posSecondCardFaceUp.value!!)?.id) {
+            cards.value?.get(posFirstCardFaceUp.value!!)?.isMatched = true
+            cards.value?.get(posSecondCardFaceUp.value!!)?.isMatched = true
+        }
+
+        cards.value?.get(posFirstCardFaceUp.value!!)?.isFaceUp = false
+        cards.value?.get(posSecondCardFaceUp.value!!)?.isFaceUp = false
+
+        posFirstCardFaceUp.value = -1
+        posSecondCardFaceUp.value = -1
+        
+        updateLayout.value = true
     }
 }
